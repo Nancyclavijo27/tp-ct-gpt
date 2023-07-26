33a78c14e384cb77b2bbe3af31cce0185be0d2d6
Nancy Clavijo Varela <99740842+Nancyclavijo27@users.noreply.github.com>
2023-07-26 12:57:20 -0500

    primer punto

--- 01 - Snippet Code/Parte 1/01-regex.js
+++ 01 - Snippet Code/Parte 1/01-regex.js
@@ -4,9 +4,25 @@
 //  - Al menos 1 número
 //  - Al menos 1 carácter especial
 
-function validarContraseña() {
-   let tuRegex; // Agrega tu regex
-   return tuRegex;
+function validarContraseña(inputString) {
+   
+      // Expresión regular que cumple con los requisitos mencionados
+      const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    
+      // Verificar si la cadena cumple con la expresión regular
+      if (regex.test(inputString)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    
+   const miCadena = "MiCadena123$";
+if (validarContraseña(miCadena)) {
+  console.log("La cadena cumple con los requisitos.");
+} else {
+  console.log("La cadena NO cumple con los requisitos.");
 }
+
 
 module.exports = validarContraseña;
